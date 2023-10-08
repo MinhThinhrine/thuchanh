5a583b03db25e5948dbdc6ae275661b4e726a792
Minh Thinh <21130549@st.hcmuaf.edu.vn>
2023-10-08 20:20:42 +0700

    bai 1 done

---
+++ th4/1/js.js
@@ -0,0 +1,19 @@
+
+function red(){
+    document.body.style.backgroundColor = "red";
+}
+function yellow(){
+    document.body.style.backgroundColor = "yellow";
+}
+function blue(){
+    document.body.style.backgroundColor = "blue";
+}
+function green(){
+    document.body.style.backgroundColor = "green";
+}
+function purple(){
+    document.body.style.backgroundColor = "purple";
+}
+function orange(){
+    document.body.style.backgroundColor = "orange";
+}
